3370d42e86651bb924e869afc7d77a7621462944
Osalumense <harkugbeosaz@gmail.com>
2022-04-24 18:05:28 +0100

    Designing user profile section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,11 +29,11 @@
             <div class="col-md-4">
                 <div class="card m-2">
                     <div class="card-header">{{ __('Navigation') }}</div>
-                        <div class="card-body bg-dark">
+                        <div class="card-body">
                             <div class="nav flex-column nav-pills mb-3 bg-opacity-50 mx-3 p-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                                 
-                                <button class="btn-reverse active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
-                                <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
+                                <button class="btn-inverse active mb-3" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+                                <button class="btn-inverse" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
                                 {{-- <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button> --}}
                             </div>
                         </div>
@@ -42,7 +42,7 @@
 
             <div class="col-md">
                 <div class="card m-2">
-                    <div class="card-header">{{ __('Welcome')  }}
+                    <div class="card-header h3">{{ __('Welcome')  }}
                         @php
                             echo '<strong>' . $name . '</strong>';
                         @endphp
@@ -50,8 +50,49 @@
 
                     <div class="card-body">
                         <div class="tab-content p-3  bg-white" id="v-pills-tabContent">
-                            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">Welcome to HCS, select the "Chat with counsellor" to begin your counselling sessions with your preferred therapist. </div>
-                            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">Loremipsum dolor dfjfidfngdd gdjgdgdfsd as asasaklas oiiufhrihsfhsf ujisnfshfuifhs </div>
+                            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                                <p>Welcome to HCS, select the "Chat with counsellor" button <span><i class="ml-2 fa-solid fa-arrow-turn-up"></i></span> to begin your counselling sessions with your preferred therapist. </p> 
+                            </div>
+                            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">Loremipsum dolor dfjfidfngdd gdjgdgdfsd as asasaklas oiiufhrihsfhsf ujisnfshfuifhs 
+                                <table>
+                                    <tr>
+                                        <td>Full Name:</td>
+                                        <td><h5>{{Auth::user()->last_name}} {{Auth::user()->first_name}}</h5></td>
+                                    </tr>
+                                </table>
+                                <form class="form-horizontal mt-3" action="#"
+                                method="POST" enctype="multipart/form-data" id="parsleyForm">
+                                @csrf
+                                <div class="row">
+                                    <div class="form-group col-sm-6">
+                                        <label for="firstname"
+                                            class="required">First name</label>
+                                        <div class="input-group {{ $errors->has('first_name') ? 'has-error' : '' }}">
+                                            <input type="text" name="first_name" class="form-control form-control" id="first_name"
+                                                placeholder="First Name" value="{{old('first_name',Auth::user()->first_name)}}" required
+                                                data-parsley-validate-name autocomplete="{dydx}">
+                                        </div>
+                                        @if ($errors->has('first_name'))
+                                            <div class="text-danger">{{ $errors->first('first_name') }}</div>
+                                        @endif
+                                    </div>
+
+                                    <div class="form-group col-sm-6">
+                                        <label for="lastname"
+                                            class="">Last name</label>
+                                        <div class="input-group {{ $errors->has('last_name') ? 'has-error' : '' }}">
+                                            <input type="text" name="last_name" class="form-control form-control" id="last_name"
+                                                placeholder="Last Name" value="{{old('last_name',Auth::user()->last_name)}}"
+                                                data-parsley-validate-name autocomplete="off">
+                                        </div>
+                                        @if ($errors->has('last_name'))
+                                            <div class="text-danger">{{ $errors->first('last_name') }}</div>
+                                        @endif
+                                    </div>
+                                </div>
+                            </form>
+                                
+                            </div>
                             {{-- <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">Loremipsum dolor dfjfidfngdd gdjgdgdfsd as asasaklas oiiufhrihsfhsf ujisnfshfuifhs </div> --}}
                         </div>
                     </div>
@@ -71,4 +112,7 @@
         
     </div>
 </section>
+@endsection
+@section('scripts')
+    <script src="https://kit.fontawesome.com/3b8c65f5c7.js" crossorigin="anonymous"></script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- <link rel="shortcut icon" type="image/x-icon" href="{{ asset('docs/favicon.png') }}"> --}}
 
     
     <title>{{ config('app.name', 'Laravel') }}</title>
